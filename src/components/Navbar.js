5757ac3704b8a7c31511fd2bf39b602024808ff5
lazikos94 <lazikos94@gmail.com>
2020-03-05 14:31:45 +0200

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext'
 
 class Navbar extends Component {
-    static contextType = ThemeContext;
     render() { 
-        console.log(this.context);
-        const {isLightTheme,light,dark} = this.context;
-        const theme = isLightTheme ? light : dark;
-        console.log(theme.ui,theme.syntax)
-        return ( 
-            <nav style={{backgroundColor: theme.ui, color: theme.syntax}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
+        return (
+            <ThemeContext.Consumer>{(context)=>{
+                const {isLightTheme,light,dark} = context;
+                const theme = isLightTheme ? light : dark;
+                return(
+                    <nav style={{backgroundColor: theme.ui, color: theme.syntax}}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
          );
     }
 }
